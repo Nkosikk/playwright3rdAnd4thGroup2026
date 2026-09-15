2ed9d398bc856640a8240b0d3325d0fa6dea6363
AvelaNoganta <avisnamsisi@gmail.com>
2026-09-15 19:13:00 +0200

    Added the testdata class

---
+++ testdata/TestData.ts
@@ -0,0 +1,12 @@
+
+
+export const validUsers={
+    admin: {
+        username: 'admin@gmail.com',
+        password: '@12345678',
+    },
+    studentUser: {
+        username: 'playtest@gmail.com',
+        password: '1234567!',
+    }
+}
