ab44ae16dfc8dd8dded6216e660c29f4e206bcff
AvelaNoganta <avisnamsisi@gmail.com>
2026-09-15 18:21:36 +0200

    Added the BasePage

---
+++ utils/BasePage.ts
@@ -0,0 +1,29 @@
+import { Locator, Page } from "@playwright/test";
+import { expect } from "@playwright/test";  
+
+export class BasePage{
+
+    constructor(public page:Page){
+    }
+
+    async GoTo(url: string){
+        await this.page.goto(url);
+    }
+
+    async ClickElement(locator: Locator){
+        await locator.click();
+    }
+    
+    async EnterText(locator: Locator, text: string){
+        await locator.fill(text);
+    }
+
+    async VerifyElementVisible(locator: Locator){
+        await expect(locator).toBeVisible();
+    }
+    async GetTextElementValue(locator: Locator){
+        return await locator.textContent();
+    }
+
+
+}
