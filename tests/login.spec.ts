bdd680bf7eb09164a135331ac88c55490f85f31f
AvelaNoganta <avisnamsisi@gmail.com>
2026-09-15 19:32:25 +0200

    Added the test class

---
+++ tests/login.spec.ts
@@ -0,0 +1,18 @@
+import { test } from "../fixtures/CustomFixtures";
+import { validUsers } from "../testdata/TestData";
+
+test.describe('Login Tests', () => {
+
+    test('Admin user should be able to login', async ({ loginPage }) => {
+        await loginPage.openNdosiPage();
+        await loginPage.navigateToLoginPage();
+        await loginPage.userLogin(validUsers.admin.username, validUsers.admin.password);
+
+    });
+     test('Student user should be able to login', async ({ loginPage }) => {
+        await loginPage.openNdosiPage();
+        await loginPage.navigateToLoginPage();
+        await loginPage.userLogin(validUsers.studentUser.username, validUsers.studentUser.password);
+
+    });
+});
