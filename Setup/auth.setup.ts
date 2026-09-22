0cfcb5d3f8cf5662da0db0fd84052c100653f197
AvelaNoganta <avisnamsisi@gmail.com>
2026-09-22 18:37:34 +0200

    Added the setup class

---
+++ Setup/auth.setup.ts
@@ -0,0 +1,20 @@
+import { LoginPage } from "../Pages/LoginPage";
+import { validUsers } from "../testdata/TestData";
+import { test as setup } from "@playwright/test";
+
+
+const authFile = 'playwright/.auth/user.json';
+
+setup('authenticate', async ({ page }) => {
+
+    const loginPage = new LoginPage(page);
+    await loginPage.openNdosiPage
+    await loginPage.GoToUrl('/');
+    await loginPage.userLogin(validUsers.admin.username, validUsers.admin.password);
+
+    // Save authentication state to file
+    await page.context().storageState({
+        path: authFile
+    });
+
+});
